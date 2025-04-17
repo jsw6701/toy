5d459e38031d114c2ea8744b890eec4c0583af08
Glenn <glenn@ecoletree.com>
2025-04-17 15:03:17 +0900

    feat : [COMMON] 스웨거 오류 수정

--- src/main/java/com/example/toy/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/toy/common/exception/GlobalExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(basePackages = {"com.example.toy"})
 public class GlobalExceptionHandler {
   @ExceptionHandler(CustomException.class)
   public ResponseEntity<Map<String, Object>> handleCustomException(CustomException ex) {
