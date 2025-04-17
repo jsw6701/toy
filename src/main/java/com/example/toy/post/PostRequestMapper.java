63f03832f9db8387e475564d3ab86fe593c479ea
Glenn <glenn@ecoletree.com>
2025-04-17 10:33:28 +0900

    feat : [COMMON] Mapper 설정

---
+++ src/main/java/com/example/toy/post/PostRequestMapper.java
@@ -0,0 +1,15 @@
+package com.example.toy.post;
+
+import com.example.toy.post.dto.req.read.PostReadAllRequestDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PostRequestMapper {
+
+    PostRequestMapper INSTANCE = Mappers.getMapper(PostRequestMapper.class);
+
+    PostReadAllRequestDto toPostReadAllRequestDto(String isDeleted);
+
+    PostReadAllRequestDto toPostReadAllRequestDto(PostReadAllRequestDto postReadAllRequestDto, String isDeleted, Integer pageNo, Integer pageRow, Integer totalCount);
+}
